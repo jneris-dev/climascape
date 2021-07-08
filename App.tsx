bda23ed494bada9b50ad5ddb765fd61f46e54fd4
jneris-wd <site@jneris.com.br>
2021-07-08 16:32:20 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text style={styles.yourLocalization}>Your Localization Now</Text>
+			<Text style={styles.localization}>Guarulhos, São Paulo, Br</Text>
+			<View>
+				<Text style={styles.icon}>ICON</Text>
+				<Text style={styles.tempNow}>20ºC</Text>
+			</View>
+			<View style={styles.wrapExtras}>
+				<View style={styles.contentExra}>
+					<Text style={styles.icoExtra}>wind</Text>
+					<Text style={styles.textExtra}>5km/h</Text>
+				</View>
+				<View style={styles.contentExra}>
+					<Text style={styles.icoExtra}>humidity</Text>
+					<Text style={styles.textExtra}>5%</Text>
+				</View>
+				<View style={styles.contentExra}>
+					<Text style={styles.icoExtra}>pressure</Text>
+					<Text style={styles.textExtra}>5km/h</Text>
+				</View>
+			</View>
+			<StatusBar style="auto" />
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+	},
+	yourLocalization: {
+
+	},
+	localization: {
+
+	},
+	icon: {
+
+	},
+	tempNow: {
+
+	},
+	wrapExtras: {
+
+	},
+	contentExra: {
+
+	},
+	icoExtra: {
+
+	},
+	textExtra: {
+
+	}
 });
